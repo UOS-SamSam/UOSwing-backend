8cc0241af48f87e99872cef2dbcc934dd8cf5d8c
Moonki Choi <52124204+iknoom@users.noreply.github.com>
2021-03-22 18:42:58 +0900

    Add PadBoxLog Entity

---
+++ backend/src/main/java/uos/samsam/wing/domain/padboxlog/PadBoxLog.java
@@ -0,0 +1,32 @@
+package uos.samsam.wing.domain.padboxlog;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import uos.samsam.wing.domain.BaseTimeEntity;
+import uos.samsam.wing.domain.padbox.PadBox;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class PadBoxLog extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "BOX_ID")
+    private PadBox padBox;          // 사용된 PadBox
+
+    private int usedAmount;         // 사용 갯수
+
+    @Builder
+    public PadBoxLog(PadBox padBox, int usedAmount) {
+        this.padBox = padBox;
+        this.usedAmount = usedAmount;
+    }
+}
---
+++ backend/src/test/java/uos/samsam/wing/domain/padboxlog/PadBoxLogRepositoryTest.java
@@ -0,0 +1,72 @@
+package uos.samsam.wing.domain.padboxlog;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uos.samsam.wing.domain.padbox.PadBox;
+import uos.samsam.wing.domain.padbox.PadBoxRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PadBoxLogRepositoryTest {
+
+    @Autowired PadBoxLogRepository padBoxLogRepository;
+    @Autowired PadBoxRepository padBoxRepository;
+
+    @AfterEach
+    public void cleanup() {
+        padBoxLogRepository.deleteAll();
+        padBoxRepository.deleteAll();
+    }
+
+    @Test
+    public void 로그_생성_불러오기() {
+        // given
+        Long boxId = 1L;
+        Double latitude = 37.583458;
+        Double longitude = 127.058305;
+        String address = "서울특별시 동대문구 전농2동 89-14";
+        String name = "21세기관";
+        int padAmount = 0;
+        Double temperature = 21.0;
+        Double humidity = 35.0;
+        PadBox padBox = PadBox.builder()
+                .boxId(boxId)
+                .latitude(latitude)
+                .longitude(longitude)
+                .address(address)
+                .name(name)
+                .padAmount(padAmount)
+                .temperature(temperature)
+                .humidity(humidity)
+                .build();
+        padBoxRepository.save(padBox);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        int usedAmount = 3;
+        padBoxLogRepository.save(PadBoxLog.builder()
+                .padBox(padBox)
+                .usedAmount(usedAmount)
+                .build());
+
+        // when
+        List<PadBoxLog> padBoxLogList = padBoxLogRepository.findAll();
+
+        // then
+        PadBoxLog padBoxLog = padBoxLogList.get(0);
+        assertThat(padBoxLog.getUsedAmount()).isEqualTo(usedAmount);
+        assertThat(padBoxLog.getCreatedDate()).isAfter(now);
+        assertThat(padBox.getBoxId()).isEqualTo(padBoxLog.getPadBox().getBoxId());
+        assertThat(padBox.getId()).isEqualTo(padBoxLog.getPadBox().getId());
+        assertThat(padBox.getName()).isEqualTo(padBoxLog.getPadBox().getName());
+    }
+
+}
