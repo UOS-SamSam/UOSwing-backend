a0312e59e2c449ae99a9be8cd237902b9e6f0660
kir3i <kirei7081@gmail.com>
2021-03-27 21:49:33 +0900

    Override configure() in WebSecurityConfigurerAdapter

--- src/main/java/uos/samsam/wing/SecurityConfig.java
+++ src/main/java/uos/samsam/wing/SecurityConfig.java
@@ -2,14 +2,30 @@ package uos.samsam.wing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+    }
+
 }
