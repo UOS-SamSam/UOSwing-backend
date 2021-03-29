19b323cc278c0d46e5a56072f82d3697e9c9dec8
kir3i <kirei7081@gmail.com>
2021-03-29 21:41:51 +0900

    Add new ReportTag classifications

--- src/main/java/uos/samsam/wing/domain/report/ReportTag.java
+++ src/main/java/uos/samsam/wing/domain/report/ReportTag.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReportTag {
-    MISSED_KEY("TAG_KEY", "키 분실"),
-    BROKEN("TAG_BROKEN", "파손");
+    KEY_MISSED("TAG_KEY_MISSED", "생리대함 키 분실"),
+    BROKEN("TAG_BROKEN", "생리대함 파손"),
+    EMPTY("TAG_NO_PAD", "생리대가 하나도 없음"),
+    WRONG_QUANTITY("TAG_WRONG_QUANTITY", "수량 오차 발생"),
+    DEFECT("TAG_DEFECT", "기타 결함");
 
     private final String key;
     private final String name;
