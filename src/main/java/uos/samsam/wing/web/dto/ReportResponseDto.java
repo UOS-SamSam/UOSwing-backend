b356f08339393dfd6608f1b023769c79a8176e53
kir3i <kirei7081@gmail.com>
2021-04-11 01:01:28 +0900

    Modify ReportResponse to include PadBox DTO instead of PadBox Entity

--- src/main/java/uos/samsam/wing/web/dto/ReportResponseDto.java
+++ src/main/java/uos/samsam/wing/web/dto/ReportResponseDto.java
@@ -11,14 +11,14 @@ import uos.samsam.wing.domain.report.ReportTag;
 public class ReportResponseDto {
 
     private Long id;
-    private PadBox padBox;
+    private PadBoxResponseDto padBox;
     private ReportTag tag;
     private String content;
     private Boolean isResolved;
 
     public ReportResponseDto(Report entity) {
         this.id = entity.getId();
-        this.padBox = entity.getPadBox();
+        this.padBox = new PadBoxResponseDto(entity.getPadBox());
         this.tag = entity.getTag();
         this.content = entity.getContent();
         this.isResolved = entity.getIsResolved();
