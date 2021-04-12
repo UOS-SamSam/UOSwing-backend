13f41e7e5c5aba01d9610812228a0178af934617
kir3i <kirei7081@gmail.com>
2021-04-12 19:13:57 +0900

    Add createdDate field and test

--- src/main/java/uos/samsam/wing/web/dto/NoticeResponseDto.java
+++ src/main/java/uos/samsam/wing/web/dto/NoticeResponseDto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import uos.samsam.wing.domain.notice.Notice;
 
+import java.time.LocalDateTime;
+
 @Getter
 @NoArgsConstructor
 public class NoticeResponseDto {
@@ -12,10 +14,12 @@ public class NoticeResponseDto {
     private Long id;
     private String title;
     private String content;
+    private LocalDateTime createdDate;
 
     public NoticeResponseDto(Notice entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.content = entity.getContent();
+        this.createdDate = entity.getCreatedDate();
     }
 }
