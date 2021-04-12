7c1a4cfd7675213e8ad81bde0c56a548d02d4a02
kir3i <kirei7081@gmail.com>
2021-04-12 20:23:29 +0900

    Add admin API description in detail

--- src/main/java/uos/samsam/wing/SwaggerConfig.java
+++ src/main/java/uos/samsam/wing/SwaggerConfig.java
@@ -2,11 +2,17 @@ package uos.samsam.wing;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -15,6 +21,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .groupName("API v1 - 20210328")
                 .select()
                 .apis(RequestHandlerSelectors.any())
--- src/main/java/uos/samsam/wing/web/UserApiController.java
+++ src/main/java/uos/samsam/wing/web/UserApiController.java
@@ -1,5 +1,9 @@
 package uos.samsam.wing.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +13,7 @@ import uos.samsam.wing.service.user.UserService;
 import uos.samsam.wing.web.dto.UserLoginRequestDto;
 import uos.samsam.wing.web.dto.UserSaveRequestDto;
 
+@Api(value = "UserApiController v1")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
 @RestController
@@ -16,11 +21,23 @@ public class UserApiController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "로그인", notes = "관리자로 로그인합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "로그인 성공"),
+            @ApiResponse(code = 400, message = "존재하지 않는 ID거나 PW가 틀림"),
+            @ApiResponse(code = 500, message = "서버 내부 에러")
+    })
     @PostMapping("/login")
     public String login(@RequestBody UserLoginRequestDto requestDto) {
         return userService.login(requestDto);
     }
 
+    @ApiOperation(value = "회원가입(테스트용)", notes = "새로운 관리자 계정을 생성합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "회원 생성 성공"),
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 500, message = "서버 내부 에러")
+    })
     @PostMapping("/join")
     public Long join(@RequestBody UserSaveRequestDto requestDto) {
         return userService.join(requestDto);
